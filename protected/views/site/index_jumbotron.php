77717556f67321a47d5f82506f9774e9677a4715
lavalain <johanna.linderoth@outlook.com>
2014-04-22 15:33:40 +0200

    nu

--- protected/views/site/index_jumbotron.php
+++ protected/views/site/index_jumbotron.php
@@ -10,7 +10,7 @@ Här på CV-Pages kan du enkelt publicera ett eller flera CV:n. Dina CV:n kan va
 
 Du som söker efter kandidater att anställa kan hitta dem här genom att söka på nyckelord och inleda en enkel rekryteringsprocess och hitta din kandidat!");?> </p>
 <br>
-        <p><a href="/cv" class="btn btn-primary btn-lg"><?php echo Yii::t("t","Sök efter CV");?></a> </p>
+        <p><a href="/cv" class="btn btn-primary btn-lg"><?php echo Yii::t("t","Sök efter CV") ("nu");?></a> </p>
     </div>
 </div>
 <h2><?php echo Yii::t('t',"3 skäl till att använda CV-Pages");?></h2>

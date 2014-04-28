3b443a265a863f4dd3d958f2991fb721353bfd80
Mattias Söderin <Mattias Sderin>
2014-04-28 10:37:25 +0200

    class ="iconbar" fixad i mobil läge

--- protected/views/layouts/jumbotron.php
+++ protected/views/layouts/jumbotron.php
@@ -7,7 +7,7 @@
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-                    <a class="navbar-brand" href="<?php echo Yii::app()->getHomeUrl();?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>n cpslass="icon-bar"></span>
+                    <a class="navbar-brand" href="<?php echo Yii::app()->getHomeUrl();?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>n class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="<?php echo Yii::app()->getHomeUrl();?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
             </div>

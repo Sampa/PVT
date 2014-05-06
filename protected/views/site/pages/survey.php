f5da33f26fdca640db6a0452d921fa047d335921
Daniel <idrini@gmail.com>
2014-05-06 10:41:24 +0200

    start för survey

--- protected/views/site/pages/survey.php
+++ protected/views/site/pages/survey.php
@@ -6,8 +6,45 @@ $this->breadcrumbs = array(
 );
 ?>
 <div class="container page-min-height">
+    <div class="row col-md-10">
+        <div class="col-md-7">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Valbara form stuff</h3>
+                </div>
+                <div class="panel-body">
+                    Panel content
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="panel panel-warning">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Trash</h3>
+                </div>
+                <div class="panel-body">
+                    Panel content
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-10">
 
-    <h1 class="page-header"><?php echo Yii::t("t", "Surveydemotesthoho");?></h1>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+                Panel content
+            </div>
+        </div>
+    </div>
+    <!--    <div class="list-group">-->
+<!--        <a href="#" class="list-group-item active">-->
+<!--            <h4 class="list-group-item-heading">List group item heading</h4>-->
+<!--            <p class="list-group-item-text"></p>-->
+<!--        </a>-->
+<!--    </div>-->
 
 </div>
 

8706e6fdd0de099252e637ccd21c10e8548447cc
Daniel <idrini@gmail.com>
2014-05-06 10:13:02 +0200

    SURVEYviem

---
+++ protected/views/site/pages/survey.php
@@ -0,0 +1,15 @@
+<?php
+$this->pageTitle = Yii::app()->name . ' - Skapa enkät';
+$this->breadcrumbs = array(
+    Yii::t("t","Hem")=>Yii::app()->getHomeUrl(),
+    Yii::t("t",'Survey'),
+);
+?>
+<div class="container page-min-height">
+
+    <h1 class="page-header"><?php echo Yii::t("t", "Surveydemotesthoho");?></h1>
+
+</div>
+
+
+

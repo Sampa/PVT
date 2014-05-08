bc957956fbb3b9939fa8ce1ee718f6005bb7ad8c
Martin Warström <martin@warstrom.se>
2014-05-08 13:08:22 +0200

    Lagt till ett till steg i guiden

--- protected/views/site/pages/survey.php
+++ protected/views/site/pages/survey.php
@@ -8,9 +8,9 @@
     $beenToSurveyPage=$recruiter->beenToSurveyPage;
 ?>
 
-<div class="container page-min-height">
+<div class="container page-min-height bootstro" data-bootstro-title="Skapa din personliga enkät" data-bootstro-content="Du använder dig av dessa fält för att bygga upp din enkät så som du vill" data-bootstro-placement="left" data-bootstro-width='150px' data-bootstro-step="0">
     <div class="row col-md-12">
-        <div style="padding: 0px;position: fixed;max-width: 180px;" class="col-md-2 panel panel-info bootstro" data-bootstro-title="Välj fråga" data-bootstro-content="Här väljer du vilken sorts fråga som du vill ha i din enkät" data-bootstro-placement="right" data-bootstro-step="0">
+        <div style="padding: 0px;position: fixed;max-width: 180px;" class="col-md-2 panel panel-info bootstro" data-bootstro-title="Välj fråga" data-bootstro-content="Här väljer du vilken sorts fråga som du vill ha i din enkät" data-bootstro-placement="right" data-bootstro-step="1">
             <div class="panel-heading">
                 <h3 class="panel-title">
                     <span class="glyphicon glyphicon-cog"></span> Komponenter
@@ -31,7 +31,7 @@
         </div>
 
         <div class="col-md-2 pull-right">
-            <div style="position: fixed;max-width: 180px;" class="panel panel-warning bootstro" data-bootstro-title="Släng saker du inte vill ha" data-bootstro-content="Detta är din papperskorg, släng saker du inte vill ha här." data-bootstro-placement="bottom" data-bootstro-width='272px' data-bootstro-step="2">
+            <div style="position: fixed;max-width: 180px;" class="panel panel-warning bootstro" data-bootstro-title="Släng saker du inte vill ha" data-bootstro-content="Detta är din papperskorg, släng saker du inte vill ha här." data-bootstro-placement="bottom" data-bootstro-width='272px' data-bootstro-step="3">
                 <div class="panel-heading">
                     <h3 class="panel-title">
                         <span class="glyphicon glyphicon-trash"></span> Papperskorg
@@ -43,7 +43,7 @@
         </div>
 
         <div class="col-md-8 pull-right" >
-            <div class="panel panel-info resize bootstro" data-bootstro-title="Bygg upp din enkät här" data-bootstro-content="Dra hit de olika sorters frågor du vill ha med i din enkät" data-bootstro-placement="left" data-bootstro-step="1">
+            <div class="panel panel-info resize bootstro" data-bootstro-title="Bygg upp din enkät här" data-bootstro-content="Dra hit de olika sorters frågor du vill ha med i din enkät" data-bootstro-placement="left" data-bootstro-step="2">
                 <div class="panel-heading">
                     <h3 class="panel-title">
                         <span class="glyphicon glyphicon-wrench "></span> Din layout

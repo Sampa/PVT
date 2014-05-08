c8d1305991178e7c426498cf3441142fb5acee25
lavalain <johanna.linderoth@outlook.com>
2014-05-08 15:52:16 +0200

    Har ändrat utseendet på dropdown i lägg till hotlist

--- protected/views/recruiter/_recruiterProcessesAsList.php
+++ protected/views/recruiter/_recruiterProcessesAsList.php
@@ -3,7 +3,7 @@
         echo "<li>".Yii::t("t","Du har inga processer nu")."</li>";
     else{
         foreach($processes as $process){
-            echo "<li>".$process->title."</li>";
+            echo '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'.$process->title."</a></li>";
         }
     }
 

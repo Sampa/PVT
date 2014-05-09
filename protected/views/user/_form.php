a8b9a7b64898912f59a450e08945c2f04f0288fb
Viktor <velysify@gmail.com>
2014-05-09 15:01:22 +0200

    Kontohantering, /user/update/idnr kan ändra fält. Däremot saknas feedback vid fel.

---
+++ protected/views/user/_form.php
@@ -0,0 +1,90 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+/* @var $form TbActiveForm */
+?>
+<div class="container page-min-height">
+    <div class="form">
+
+        <?php $form=$this->beginWidget('CActiveForm', array(
+    	'id'=>'user-form',
+    	// Please note: When you enable ajax validation, make sure the corresponding
+    	// controller action is handling ajax validation correctly.
+    	// There is a call to performAjaxValidation() commented in generated controller code.
+    	// See class documentation of CActiveForm for details on this.
+    	'enableAjaxValidation'=>true,
+    )); ?>
+
+        <p class="help-block">Fields with <span class="required">*</span> are required.</p>
+
+             <div class="form-group">
+                <?php echo $form->labelEx($model, 'username', array('class' => 'col-lg-12 control-label')); ?>
+                <div class="col-lg-5">
+                    <?php echo $form->textField($model, 'username', array('class' => 'form-control', 'placeholder' => Yii::t("t", "Välj ett användarnamn"))); ?>
+                    <div class="help-block">
+                        <?php echo $form->error($model, 'username'); ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <?php echo $form->labelEx($model, 'fullname', array('class' => 'col-lg-12 control-label')); ?>
+                <div class="col-lg-5">
+                    <?php echo $form->textField($model, 'fullname', array('class' => 'form-control', 'placeholder' => Yii::t("t", "Fyll i fullständigt namn"))); ?>
+                    <span class="help-block help-inline ">
+                    <?php echo $form->error($model, 'fullname'); ?>
+                        </span>
+                </div>
+            </div>
+            <div class="form-group ">
+                <?php echo $form->labelEx($model, 'email', array('class' => 'col-lg-12 control-label')); ?>
+                <div class="col-lg-5">
+                    <?php echo $form->textField($model, 'email', array('class' => 'form-control', 'placeholder' => Yii::t("t", "Fyll i din E-post"))); ?>
+                    <span class="help-block help-inline ">
+                    <?php echo $form->error($model, 'email'); ?>
+                        </span>
+                </div>
+            </div>
+            <div class="form-group">
+                <?php echo $form->labelEx($model, 'new_password', array('class' => 'col-lg-12 control-label')); ?>
+                <div class="col-lg-5">
+                    <?php echo $form->passwordField($model, 'new_password', array('class' => 'form-control', 'placeholder' => Yii::t("t", "Nytt lösenord"))); ?>
+                    <div class="help-block">
+                        <?php echo $form->error($model, 'new_password'); ?>
+                    </div>
+                </div>
+            </div>
+
+             <div class="form-group" >
+                <?php echo $form->labelEx($model, 'password_confirm', array('class' => 'col-lg-12 control-label')); ?>
+                <div class="col-lg-5">
+                  <?php echo $form->passwordField($model, 'password_confirm', array('type' => 'password', 'class' => 'form-control', 'placeholder' => Yii::t("t", "Bekräfta nytt lösenord"), 'rows' => 6, 'cols' => 50)); ?>
+                  <div class="help-block">
+                    <?php echo $form->error($model, 'password_confirm'); ?>
+                   </div>
+                </div>
+             </div>
+
+            <div class="form-group row" style ="margin-left: 1px; clear:both;">
+                <?php echo $form->labelEx($model, 'notify', array('class' => 'col-lg-1 control-label')); ?>
+                <div class="col-lg-1">
+                    <?php echo $form->checkBox($model, 'notify', array('id'=>'notify')); ?>
+                    <div class="help-block">
+                        <?php echo $form->error($model, 'notify'); ?>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="controls  col-lg-12">
+                <div class="form-actions row" style ="margin-left: 0px">
+                    <?php echo TbHtml::submitButton($model->isNewRecord ? Yii::t("t", "Updatera användare") : Yii::t("t", "Spara"),array(
+                      'color'=>TbHtml::BUTTON_COLOR_PRIMARY,
+                      'size'=>TbHtml::BUTTON_SIZE_LARGE,)); ?>
+                </div>
+            </div>
+
+        <?php $this->endWidget(); ?>
+
+    </div><!-- form -->
+</div>
---
+++ protected/views/user/update.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+?>
+
+<?php
+$this->breadcrumbs=array(
+	'Users'=>array('index'),
+	$model->name=>array('view','id'=>$model->id),
+	'Update',
+);
+
+$this->menu=array(
+	array('label'=>'List User', 'url'=>array('index')),
+	array('label'=>'Create User', 'url'=>array('create')),
+	array('label'=>'View User', 'url'=>array('view', 'id'=>$model->id)),
+	array('label'=>'Manage User', 'url'=>array('admin')),
+);
+?>
+
+    <div class="page-header">
+        <h1><?php echo Yii::t("t", "Uppdatera användaruppgifter");?></h1>
+    </div>
+
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>

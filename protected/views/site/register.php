5b9df4ecf41a2b7882ac14138fd2a214793ccab9
Mattias Söderin <Mattias Sderin>
2014-05-15 13:50:03 +0200

    Modal ruta när man klickar på "Läs användaravtal"

--- protected/views/site/register.php
+++ protected/views/site/register.php
@@ -138,7 +138,7 @@ $this->breadcrumbs = array(
 
                 </div>
             </div>
-            <div class="col-lg-3"><a href="<?php echo Yii::app()->baseUrl;?>/site/page/agreement    ">Läs användaravtal</a></div>
+            <div class="col-lg-3"><a href="" data-toggle="modal" data-target="#agreementPopup">Läs användaravtal</a></div>
 
         </div>
 
@@ -152,5 +152,37 @@ $this->breadcrumbs = array(
         <?php $this->endWidget(); ?>
     </div>
     <!-- form -->
+</div>
+
+<div class="modal fade" id="agreementPopup" tabindex="-1" role="dialog" aria-labelledby="agreementPopupLabel" aria-hidden="true">
+<div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h2 class="modal-title" id="agreementPopupLabel">Användaravtal</h2>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div>
+                    <p><?php echo t("CV-Pages är en webbplats avsedd att användas endast till jobbrekryteringar och får inte användas i andra avseenden.
+                     Detta innebär att det endast är CV:n som får laddas upp av publicerare. Vid missbruk kan ditt konto komma att stängas av.")?>
+                    </p>
+
+                    <p><?php echo t("CV-Pages förbehåller sig rätten att undanhålla och ta bort material som finns på ditt konto,utan förhandsbesked,
+                                 om CV-Pages anser att materialet strider mot detta avtal. CV-Pages har inga skyldigheter att förvara, spara eller förse med kopior av material
+                                 som du tillhandahåller när du använder CV-Pages tjänster.")?>
+                    </p>
+                    <p><?php echo t("CV-Pages ansvarar inte för det material som läggs upp och ansvarar inte heller för något som kan förekomma vid kontakt mellan publicerare och rekryterare.
+                                    CV-Pages garanterar inte anställning till publicerare som har nått en överenskommelse med rekryterare.
+                                Det finns möjlighet för användare att rapportera om olämpligt material förekommer. Dock får missbruk av denna funktion inte förekomma.
+                                Vid missbruk kan ditt konto komma att stängas av.");?>
+                    </p>
+                </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>
+        </div>
+    </div>
+</div>
 </div>
 

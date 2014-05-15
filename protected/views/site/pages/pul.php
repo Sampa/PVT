37b8115e077febf1212ec82fc86c4a119ab232ab
Mattias Söderin <Mattias Sderin>
2014-05-15 14:12:56 +0200

    Ändrat layout på Pul-sidan

--- protected/views/site/pages/pul.php
+++ protected/views/site/pages/pul.php
@@ -2,15 +2,29 @@
 $this->pageTitle = Yii::app()->name . ' - About';
 $this->breadcrumbs = array(
     Yii::t("t","Hem")=>Yii::app()->getHomeUrl(),
-    Yii::t("t",'Om oss'),
+    Yii::t("t",'PuL'),
 );
 ?>
 <div class="container page-min-height">
 
-    <h1 class="page-header"><?php echo Yii::t("t", "Personuppgiftslagen");?></h1>
+    <h1 class="page-header"> <?php echo Yii::t("t", "Personuppgiftslagen");?></h1>
+    <div class="row">
+        <div class="col-md-6">
+            <p id="texts"><?php echo t("Registrering hos CV-Pages innebär att du samtycker till att CV-Pages behandlar dina personuppgifter
+                            enligt personuppgiftslagen (1998:204 PuL).Som personuppgifter räknas all slags information som direkt eller indirekt
+                            kan hänföras till dig som fysisk person till exempel personnummer och adress. Du väljer själv om du vill lämna dina
+                             uppgifter till oss och har även rätt att vid behov korrigera de uppgifter som har lämnats. Vi kommer inte att lämna ut
+                             dina uppgifter till tredje part utan ditt samtycke.")?>
+            </p>
 
-    <p><?php echo Yii::t("t", "Vår affärside går ut på att presentera en publik gemensam databas för cv:n oavsett vilken branch du är intresserad av eller proffessionell inom. Möjligheten att vara anonym ger dig en chans till en objektiv första bedömning vilket är helt valfritt. Personliga och känsliga uppgifter som du som publicerare placerar i dina cv-filer skyddas inte av oss utan du är personligt ansvarig för att skydda dessa uppgifter.");?> <br><br> </p>
-</div>
+
+        </div>
+        <div class="col-md-4">
+            <p>
+                <img src="http://lorempixel.com/output/food-h-c-421-652-9.jpg">
+            </p>
+        </div>
+    </div>
 
 
 

e425dbad3bb9d619ece0b3aa0067815b5ce37c45
Christian Ragnarsson <Christian.ragnarsson@me.com>
2014-05-16 11:55:33 +0200

    Löste ett problem med en modal

--- protected/views/cv/index.php
+++ protected/views/cv/index.php
@@ -69,6 +69,7 @@ if($resultCount< 1):?>
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                             <h4 class="modal-title" id="reportModalLabel"><?php echo Yii::t("t","Rapportera CV");?></h4>
                         </div>
                         <div class="modal-body">

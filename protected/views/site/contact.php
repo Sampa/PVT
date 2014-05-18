b1a5d774d18a8a9d5f5fee01566f0062fb66f2b5
Daniel van den Berg <idrini@gmail.com>
2014-05-18 20:28:57 +0200

    kontaktsidans layout från default->idéförslag

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -7,7 +7,7 @@ if($_SERVER['REQUEST_URI']!="/" && $_SERVER['REQUEST_URI']!="/Group1/"){
 	);
 }
 ?>
-<div class="container">
+<div class="container ">
     <?php if (Yii::app()->user->hasFlash('contact')): ?>
         <div class="alert alert-info  alert-dismissable">
             <a class="close" data-dismiss="alert" href="#" aria-hidden="true">&times;</a>
@@ -17,14 +17,12 @@ if($_SERVER['REQUEST_URI']!="/" && $_SERVER['REQUEST_URI']!="/Group1/"){
 <!--        <div class="page-header">-->
 <!--            <h1>--><?php //echo Yii::t("t", "Kontakt");?><!-- </h1>-->
 <!--        </div>-->
-        <div class="horizontal-form">
-
+        <div class="col-lg-12 col-md-12">
             <?php $form = $this->beginWidget('CActiveForm', array(
                 'enableClientValidation' => true,
                 //'enableAjaxValidation'=>true,
                 // 'errorMessageCssClass'=>'has-error',
-
-                'htmlOptions' => array('class' => 'form-horizontal',
+                'htmlOptions' => array('class' => '',
                     'role' => 'form',
                     'id' => 'contact-form'
                 ),
@@ -36,66 +34,57 @@ if($_SERVER['REQUEST_URI']!="/" && $_SERVER['REQUEST_URI']!="/Group1/"){
                     'validateOnChange' => true
                 ),
             )); ?>
-            <div class="form-group">
-                <div class="col-lg-3 control-label">
-                    <div>
-                        <p class="note"> <?php echo Yii::t("t", "Fält markerade med");?> <span class="required">*</span> <?php echo Yii::t("t", "är obligatoriska");?>.</p>
-                    </div>
-                </div>
 
-                <div class="col-lg-5  has-error">
-                    <div class="help-block ">
-                        <?php echo $form->errorSummary($model); ?>
-                    </div>
-                </div>
-            </div>
+	            <div class="form-group">
+	                <div class="col-lg-12  has-error">
+	                    <div class="help-block ">
+	                        <?php echo $form->errorSummary($model); ?>
+	                    </div>
+	                </div>
+	            </div>
 
+	        <section class="col-lg-3 col-md-3">
+	            <div class="form-group">
+	                <?php echo $form->labelEx($model, 'name', array('class' => ' control-label')); ?>
+	                    <?php echo $form->textField($model, 'name', array('class' => 'form-control', 'placeholder' =>  Yii::t("t", "Namn"))); ?>
+	                    <div class="help-block">
+	                        <?php echo $form->error($model, 'name'); ?>
+	                    </div>
+	            </div>
 
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'name', array('class' => 'col-lg-3 control-label')); ?>
-                <div class="col-lg-5">
-                    <?php echo $form->textField($model, 'name', array('class' => 'form-control', 'placeholder' =>  Yii::t("t", "Namn"))); ?>
-                    <div class="help-block">
-                        <?php echo $form->error($model, 'name'); ?>
-                    </div>
-                </div>
-            </div>
+	            <div class="form-group">
+	                <?php echo $form->labelEx($model, 'email', array('class' => ' control-label')); ?>
+	                    <?php echo $form->textField($model, 'email', array('class' => 'form-control', 'placeholder' => Yii::t("t", "E-post"))); ?>
+	                    <span class="help-block help-inline ">
+	                <?php echo $form->error($model, 'email'); ?>
+	                    </span>
+	            </div>
 
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'email', array('class' => 'col-lg-3 control-label')); ?>
-                <div class="col-lg-5">
-                    <?php echo $form->textField($model, 'email', array('class' => 'form-control', 'placeholder' => Yii::t("t", "E-post"))); ?>
-                    <span class="help-block help-inline ">
-                <?php echo $form->error($model, 'email'); ?>
-                    </span>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'subject', array('class' => 'col-lg-3 control-label')); ?>
-                <div class="col-lg-5">
-                    <?php echo $form->textField($model, 'subject', array('class' => 'form-control', 'placeholder' => Yii::t("t", "Ämne"))); ?>
-                    <div class="help-block">
-                        <?php echo $form->error($model, 'subject'); ?>
-                    </div>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'body', array('class' => 'col-lg-3 control-label')); ?>
-                <div class="col-lg-5">
-                    <?php echo $form->textArea($model, 'body', array('class' => 'form-control', 'placeholder' => Yii::t("t", "Skriv meddelande här"), 'rows' => 6, 'cols' => 50)); ?>
+	            <div class="form-group">
+	                <?php echo $form->labelEx($model, 'subject', array('class' => 'control-label')); ?>
+	                    <?php echo $form->textField($model, 'subject', array('class' => 'form-control', 'placeholder' => Yii::t("t", "Ämne"))); ?>
+	                    <div class="help-block">
+	                        <?php echo $form->error($model, 'subject'); ?>
+	                    </div>
+	            </div>
+		    </section>
+	        <section class="col-lg-6 col-md-6">
+	        <div class="form-group">
+                <?php echo $form->labelEx($model, 'body', array('class' => 'control-label')); ?>
+                    <?php echo $form->textArea($model, 'body', array(
+	                    'class' => 'form-control', 'placeholder' => Yii::t("t", "Skriv meddelande här"), 'rows' => 9, 'cols' => 60)
+                    ); ?>
                     <div class="help-block">
                         <?php echo $form->error($model, 'body'); ?>
                     </div>
-                </div>
             </div>
 
 
             <div class="form-group">
-                <div class="col-lg-offset-3 col-lg-10">
+                <div class="">
                     <?php if ($model->getRequireCaptcha()) : ?>
-                        <?php $this->widget('application.extensions.recaptcha.EReCaptcha',
+	                    <?php echo $form->labelEx($model, 'verify_code', array('class' => 'control-label')); ?>
+	                    <?php $this->widget('application.extensions.recaptcha.EReCaptcha',
                             array('model' => $model, 'attribute' => 'verify_code',
                                 'theme' => 'red', 'language' => 'en',
                                 'publicKey' => Yii::app()->params['recaptcha_public_key']));?>
@@ -103,13 +92,29 @@ if($_SERVER['REQUEST_URI']!="/" && $_SERVER['REQUEST_URI']!="/Group1/"){
                     <?php endif; ?>
                 </div>
             </div>
-
-
+			</section>
+	        <section class="col-lg-3 col-md-3">
             <div class="form-group">
-                <div class="col-lg-offset-3 col-lg-10">
-                    <?php echo CHtml::submitButton(Yii::t("t", "Skicka"), array('class' => 'btn btn-primary btn-lg')); ?>
+                <div class="">
+	                <form>
+		                <legend><span class="glyphicon glyphicon-globe"></span> <?=t("Våra uppgifter");?></legend>
+		                <address>
+			                <strong>Cv-Pages</strong><br>
+			                795 Folsom Ave, Suite 600<br>
+			                San Francisco, CA 94107<br>
+			                <abbr title="Phone">
+				                P:</abbr>
+			                (123) 456-7890
+		                </address>
+		                <address>
+			                <strong>Full Name</strong><br>
+			                <a href="mailto:#">first.last@example.com</a>
+		                </address>
+	                </form>
+                    <?php echo CHtml::submitButton(Yii::t("t", "Skicka"), array('class' => 'btn btn-success btn-lg')); ?>
                 </div>
             </div>
+		    </section>
             <?php $this->endWidget(); ?>
         </div><!-- form -->
     <?php endif;?>

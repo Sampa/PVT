dbc4e4a9674b0f9945664ca688dfa0ae258012de
sofie0 <sofie.lofman@gmail.com>
2014-05-20 15:08:19 +0200

    mailet som ska skickas ut när man får ett meddelande på sidan

---
+++ protected/views/mailer/notifier.php
@@ -0,0 +1,18 @@
+<center>
+    <p style="font-size: 25px; margin-left: auto;  margin-right: auto;width:580px;">
+        <?php echo Yii::t("t", "Hej".$username."Du har fått ett meddelande på CV-Pages, logga in för att läsa det.");?>
+    </p>
+    <div style=" display: block;
+                               -moz-border-radius:3px;
+                               -webkit-border-radius:3px;
+                                background-color:#1aa0ff;
+                                 border-radius:3px;
+                                 margin-left: auto;
+                                  margin-right: auto;
+                                 width:180px;
+                                ">
+        <a style="color:#ffdead; font-size: 40px;text-decoration: none" href="pvt.dsv.su.se/Group1"
+           target="_blank">CV-Pages</a>
+    </div>
+</center>
+

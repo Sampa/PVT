3517e67b97e7351d8fd68c082a3171471ea997a0
Daniel <idrini@gmail.com>
2014-05-26 21:40:43 +0200

    Fixes #63

--- protected/views/user/update.php
+++ protected/views/user/update.php
@@ -3,14 +3,13 @@
 /* @var $model User */
 ?>
 <?php
-$this->breadcrumbs=array(
-	t('Användare')=>array('index'),
-	$model->name=>array('view','id'=>$model->id),
-	t('Updatera användaruppgifter'),
-);
+    $this->breadcrumbs=array(
+        t('Användare')=>array('index'),
+        t('Uppdatera användaruppgifter')=>array('view','id'=>$model->id),
+    );
 ?>
-    <div class="page-header">
-        <h1><?php echo Yii::t("t", "Uppdatera användaruppgifter");?></h1>
-    </div>
+<div class="page-header">
+    <h1><?php echo Yii::t("t", "Uppdatera användaruppgifter");?></h1>
+</div>
 
 <?php $this->renderPartial('_form', array('model'=>$model, 'rmodel' => $rmodel,)); ?> 

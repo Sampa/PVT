74010ea26dc58f31c20ed23f0aa9e5bfe7ed4818
sofie0 <sofie.lofman@gmail.com>
2014-05-26 10:21:38 +0200

    testar våra tester för jenkins igen

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ echo getcwd();
 echo "Printed paths";
 require_once('/../yii/framework/yii.php');
 $yiit=dirname(__FILE__).'/../yii/framework/yiit.php';
-$config=dirname(__FILE__).'/unit/RemoteConnectTest.php';
+$config=dirname(__FILE__).'./unit/RemoteConnectTest.php';
 //$_SERVER['REQUEST_URI'] = 'index-test.php';
 
 //require_once($yiit);

e7addfa25be25f91fd4fbbcc81b439c57db3b642
Daniel <idrini@gmail.com>
2014-05-27 20:54:16 +0200

    lite tydligare kommentarer för visa pdfer

--- protected/views/cv/_displayPdf.php
+++ protected/views/cv/_displayPdf.php
@@ -1,15 +1,16 @@
 <?php
 /*
- * @model = ett cv object
+ * $model = ett cv object
  *
  */
 ?>
-<div id="pdf<?php echo $model->id;?>" class="embeddedPdf col-md-8 col-lg-8 col-sm-4">
-
-</div>
+<!-- diven som pdf:en kommer laddas in/visas i)-->
+<div id="pdf<?php echo $model->id;?>" class="embeddedPdf col-md-8 col-lg-8 col-sm-4"></div>
 <span class="clearfix"></span>
 
+<!-- CSS kod vi ska flytta till main.css när alla är nöjda med utseendet-->
 <style>
+
     .embeddedPdf {
         margin: 2em auto;
         height: 500px;
@@ -18,7 +19,6 @@
     .embeddedPdf p {
         padding: 1em;
     }
-
     .embeddedPdf object {
         display: block;
         border: solid 1px #666;

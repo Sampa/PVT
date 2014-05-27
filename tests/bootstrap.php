20eb95aaa76de457cec0e01462257be210ed6ead
sofie0 <sofie.lofman@gmail.com>
2014-05-27 08:44:25 +0200

    test för jenkins

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,14 @@
 echo "Printed paths: ";
 echo getcwd();
 
-require_once('/../yii/framework/yii.php');
+
+require(dirname(__FILE__).'/../yii/framework/yii.php');
+//hey I just wrote this
+//and this is crazy
+//but here's some new code
+//so please no errors
+
+//require_once('/../yii/framework/yii.php');
 $yiit=dirname(__FILE__).'/../yii/framework/yiit.php';
 //$config=dirname(__FILE__).'/../protected/models/CV.php';
 

e148d98109638f5cfd3c2eee6db8fc3023cb11e4
Martin Warström <martin@warstrom.se>
2014-05-28 14:32:52 +0200

    Nu listas land osv för CN:n i Hotlist

--- protected/views/recruitmentprocess/_hotlistview.php
+++ protected/views/recruitmentprocess/_hotlistview.php
@@ -15,12 +15,14 @@
            <li><i class="glyphicon glyphicon-briefcase"></i> <span><?php echo CHtml::encode($data->cv->typeOfEmployment); ?></span></li>
             <?php
             //DB Query to get the country name.
-            if(isset($data->cv->geographicArea->country)){
-                $countryName = $data->cv->geographicArea->country;
-            }else
-                $countryName = null;
-            ?>
-            <li><i class="glyphicon glyphicon-globe"></i> <span><?= $countryName; ?></span></li>
+                        foreach($data->cv->areas as $area){?>
+                        <li>
+                            <i class="glyphicon glyphicon-globe"></i>
+                            <span><?= $area->country; ?>, </span>
+                            <span><?= $area->region; ?>, </span>
+                            <span><?= $area->city; ?></span>
+                        </li>
+            <?php }?>
            <li><i class="glyphicon glyphicon-user"></i> <span><?php echo CHtml::encode($data->cv->publisher->username); ?></span></li>
         </ul>
 

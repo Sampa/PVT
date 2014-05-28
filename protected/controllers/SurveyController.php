5419b54e34072a6a2e1fb578368b450ee81df4fe
Christian Ragnarsson <Christian.ragnarsson@me.com>
2014-05-28 11:33:45 +0200

    Nya accessRules för survey. Man måste nu vara recruiter för att komma åt dessa sidor.

--- protected/controllers/SurveyController.php
+++ protected/controllers/SurveyController.php
@@ -26,17 +26,13 @@ class SurveyController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','admin','delete','sendOutSurveys','respond'),
-				'users'=>array('@'),
+				'actions'=>array('index','view','create','update','admin','delete','sendOutSurveys'),
+				'expression'=>'(( User::model()->findByPk(Yii::app()->user->id) != null && User::model()->findByPk(Yii::app()->user->id)->isRecruiter()',
 			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+			array('allow', // allow authed users to perform 'respond' actions
+				'actions'=>array('respond'),
+				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),

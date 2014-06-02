33bc57453b7bb66c7f15b9dcce3368ba6e248cb6
Daniel <idrini@gmail.com>
2014-06-02 10:49:25 +0200

    langpicker

--- protected/components/views/langBox.php
+++ protected/components/views/langBox.php
@@ -1,12 +1,27 @@
-<?php echo CHtml::form(); ?>
-	<div id="langdrop">
+<?php echo CHtml::form("/","post",array("id"=>"langform")); ?>
+	<div id="langdrop" class="pull-left">
 		<?php
-        echo CHtml::dropDownList('_lang', $currentLang, array(
-			'en' => 'English',
-			'sv' => 'Svenska',
-			'de' => 'Rappakalja',
-		)); ?>
-		<button><?=t("Byt språk");?></button>
-	</div>
 
+        echo CHtml::dropDownList('_lang', $currentLang, array(
+            'sv'=>t(' Svenska'),
+            'en' =>t(' Engelska'),
+        ),array(
+            'options'=>array(
+              'sv'=>array('class'=>'sv'),
+              'en'=>array('class'=>'en'),
+            ),
+            'class'=>'form-control',
+            'style'=>'width:120px;',
+            'id'=>'langPicker'
+        )); ?>
+	</div>
 <?php echo CHtml::endForm(); ?>
+<script>
+$(document).ready(function(){
+     $(".sv").prepend("<div class='svFlag'></div>");
+     $(".en").prepend("<div class='enFlag'></div>");
+    $("#langPicker").on("change",function(){
+        $("#langform").submit();
+    });
+});
+</script>

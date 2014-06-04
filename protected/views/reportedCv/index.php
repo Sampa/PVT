2fc11c0a4b84a07f355363dbb88f7a51af1aea28
Christian Ragnarsson <Christian.ragnarsson@me.com>
2014-06-04 18:23:51 +0200

    Hämtar nu namn för den som rapporterade ett cv istället för bara id

--- protected/views/reportedCv/index.php
+++ protected/views/reportedCv/index.php
@@ -7,24 +7,24 @@ $this->breadcrumbs=array(
     );
     ?>
     <div class="container">
-       <div class="row clearfix">
-          <div class="col-md-12 column">
-            <div class="panel panel-info">
-             <div class="panel-heading">
-                 <h3><?php echo Yii::t('t','Rapporterade CV:n');?></h3>
-             </div>
-             <?php
-             if (count($allModels) == 0) {
-                 echo "<h4 style='text-align: center;''>Det finns inga rapporterade CV:n</h4>";
-             }else {
-             ?>
-             <div class="table-responsive">
-                 <table class="table table-bordered table-condensed table-hover">
+     <div class="row clearfix">
+      <div class="col-md-12 column">
+        <div class="panel panel-info">
+           <div class="panel-heading">
+               <h3><?php echo Yii::t('t','Rapporterade CV:n');?></h3>
+           </div>
+           <?php
+           if (count($allModels) == 0) {
+               echo "<h4 style='text-align: center;''>Det finns inga rapporterade CV:n</h4>";
+           }else {
+               ?>
+               <div class="table-responsive">
+                   <table class="table table-bordered table-condensed table-hover">
                     <tbody>
                         <tr>
                             <th><?php echo Yii::t('t','Rapporteringsdatum');?></th>
                             <th><?php echo Yii::t('t','Rapporterad av');?></th>
-                            <th><?php echo Yii::t('t','CVId');?></th>
+                            <th><?php echo Yii::t('t','CV-ID');?></th>
                             <th><?php echo Yii::t('t','Anledning');?></th>
                             <th><?php echo Yii::t('t','Radera CV');?></th>
                             <th><?php echo Yii::t('t','Rapportera CV som OK');?></th>
@@ -33,14 +33,15 @@ $this->breadcrumbs=array(
                         foreach($allModels as $model){
                             ?>
                             <tr id="<?php echo $model->cvId;?>"> 
-
-
                                 <td class="onClick"><?php
                                 echo substr($model->date, 0, 10);
                                 ?>
                             </td>
-                            <td class="onClick"><?php
-                            echo $model->reportedBy;
+                            <td class="onClick">
+                            <?php
+                                $criteria = new CDbCriteria;
+                                $criteria->compare('id', $model->reportedBy);
+                                echo User::model()->find($criteria)->username;
                             ?>
                         </td>
                         <td class="onClick"><?php

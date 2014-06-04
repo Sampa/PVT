3e6baa9160a0de575ac91d4f536cd1a33c4983c3
Mattias Söderin <Mattias Sderin>
2014-06-04 11:09:45 +0200

    redigerat i texten

--- protected/views/site/pages/pul.php
+++ protected/views/site/pages/pul.php
@@ -9,7 +9,7 @@ $this->breadcrumbs = array(
     <div class="row">
         <div class="col-md-6">
             <p id="texts"><?php echo t("Registrering hos CV-Pages innebär att du samtycker till att CV-Pages behandlar dina personuppgifter
-                            enligt personuppgiftslagen (1998:204 PuL).Som personuppgifter räknas all slags information som direkt eller indirekt
+                            enligt personuppgiftslagen (1998:204 PuL). Som personuppgifter räknas all slags information som direkt eller indirekt
                             kan hänföras till dig som fysisk person till exempel personnummer och adress. Du väljer själv om du vill lämna dina
                              uppgifter till oss och har även rätt att vid behov korrigera de uppgifter som har lämnats. Vi kommer inte att lämna ut
                              dina uppgifter till tredje part utan ditt samtycke.")?>

4fda14b4a756a208bb980a0b7622ec49795513e5
lavalain <johanna.linderoth@outlook.com>
2014-06-04 11:43:06 +0200

    ändringar i användaruppgifter

--- protected/views/site/pages/agreement.php
+++ protected/views/site/pages/agreement.php
@@ -5,9 +5,10 @@ $this->breadcrumbs = array(
     Yii::t("t",'Användaravtal'),
 );
 ?>
-<div class="container page-min-height">
+<div class="container page-min-height visible-xs">
     <div class="row">
     <div class="col-md-6">
+      <h2><?php echo t("Användaravtal");?></h2>
          <p id="texts"><?php echo t("CV-Pages är en webbplats avsedd att användas endast till jobbrekryteringar och får inte användas i andra avseenden.
           Detta innebär att det endast är CV:n som får laddas upp av publicerare. Vid missbruk kan ditt konto komma att stängas av.")?>
             </p>
@@ -22,16 +23,40 @@ $this->breadcrumbs = array(
                                 Vid missbruk kan ditt konto komma att stängas av.");?>
          </p>
     </div>
-        <div class="col-md-4">
+        <div class="col-md-6">
+            <!--<p>
+               <img src="<?php echo Yii::app()->baseUrl?>/img/myror.jpg"> 
+            </p>-->
+        </div>
+    </div>
+    </div>
+
+<div class="container page-min-height hidden-xs">
+    <div class="row">
+    <div class="col-md-6">
+      <h2><?php echo t("Användaravtal");?></h2>
+         <p id="texts"><?php echo t("CV-Pages är en webbplats avsedd att användas endast till jobbrekryteringar och får inte användas i andra avseenden.
+          Detta innebär att det endast är CV:n som får laddas upp av publicerare. Vid missbruk kan ditt konto komma att stängas av.")?>
+            </p>
+
+        <p id="texts"><?php echo t("CV-Pages förbehåller sig rätten att undanhålla och ta bort material som finns på ditt konto,utan förhandsbesked,
+                om CV-Pages anser att materialet strider mot detta avtal. CV-Pages har inga skyldigheter att förvara, spara eller förse med kopior av material
+                som du tillhandahåller när du använder CV-Pages tjänster.")?>
+         </p>
+    <p id="texts"><?php echo t("CV-Pages ansvarar inte för det material som läggs upp och ansvarar inte heller för något som kan förekomma vid kontakt mellan publicerare och rekryterare.
+                                    CV-Pages garanterar inte anställning till publicerare som har nått en överenskommelse med rekryterare.
+                                Det finns möjlighet för användare att rapportera om olämpligt material förekommer. Dock får missbruk av denna funktion inte förekomma.
+                                Vid missbruk kan ditt konto komma att stängas av.");?>
+         </p>
+    </div>
+        <div class="col-md-6">
             <p>
-               <img src="http://http://lorempixel.com/400/200/"> 
+               <img src="<?php echo Yii::app()->baseUrl?>/img/myror.jpg"> 
             </p>
         </div>
     </div>
     </div>
 
-
-</div>
 
 
 
--- protected/views/site/pages/pul.php
+++ protected/views/site/pages/pul.php
@@ -2,27 +2,46 @@
 $this->pageTitle = Yii::app()->name . ' - Personuppgifter';
 $this->breadcrumbs = array(
     Yii::t("t","Hem")=>Yii::app()->getHomeUrl(),
-    Yii::t("t",'PuL'),
+    Yii::t("t",'Personuppgifter'),
 );
 ?>
-<div class="container page-min-height">
+<div class="container page-min-height hidden-xs">
     <div class="row">
+        <h2><?php echo Yii::t("t", "Personuppgifter");?></h2>
         <div class="col-md-6">
             <p id="texts"><?php echo t("Registrering hos CV-Pages innebär att du samtycker till att CV-Pages behandlar dina personuppgifter
                             enligt personuppgiftslagen (1998:204 PuL).Som personuppgifter räknas all slags information som direkt eller indirekt
                             kan hänföras till dig som fysisk person till exempel personnummer och adress. Du väljer själv om du vill lämna dina
                              uppgifter till oss och har även rätt att vid behov korrigera de uppgifter som har lämnats. Vi kommer inte att lämna ut
-                             dina uppgifter till tredje part utan ditt samtycke.")?>
-            </p>
-
+                             dina uppgifter till tredje part utan ditt samtycke.")?></p>
 
         </div>
         <div class="col-md-4">
             <p>
-                <img src="http://lorempixel.com/output/food-h-c-421-652-9.jpg">
+                <img src="<?php echo Yii::app()->baseUrl?>/img/bron.jpg">
             </p>
         </div>
     </div>
+</div>
+
+<div class="container page-min-height visible-xs">
+    <div class="row">
+        <h2><?php echo Yii::t("t", "Personuppgifter");?></h2>
+            <div class="col-md-6">
+            <p id="texts"><?php echo t("Registrering hos CV-Pages innebär att du samtycker till att CV-Pages behandlar dina personuppgifter
+                            enligt personuppgiftslagen (1998:204 PuL).Som personuppgifter räknas all slags information som direkt eller indirekt
+                            kan hänföras till dig som fysisk person till exempel personnummer och adress. Du väljer själv om du vill lämna dina
+                             uppgifter till oss och har även rätt att vid behov korrigera de uppgifter som har lämnats. Vi kommer inte att lämna ut
+                             dina uppgifter till tredje part utan ditt samtycke.")?></p>
+
+        </div>
+        <div class="col-md-4">
+           <!-- <p>
+                <img src="<?php echo Yii::app()->baseUrl?>/img/anvbild.jpg">
+            </p>-->
+        </div>
+    </div>
+</div>
 
 
 

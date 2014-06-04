0fc0305ab887f4a383b13fa5b27dd83f74daee82
Christian Ragnarsson <Christian.ragnarsson@me.com>
2014-06-04 17:42:03 +0200

    Förbättrade designen av rapporterade cv vyn

--- protected/views/reportedCv/index.php
+++ protected/views/reportedCv/index.php
@@ -9,63 +9,61 @@ $this->breadcrumbs=array(
     <div class="container">
        <div class="row clearfix">
           <div class="col-md-12 column">
-             <div class="panel-heading" style='margin-left:-16px;'>
+            <div class="panel panel-info">
+             <div class="panel-heading">
                  <h3><?php echo Yii::t('t','Rapporterade CV:n');?></h3>
              </div>
-            <div class="table-responsive">
-             <table class="table table-bordered table-condensed table-hover">
+             <div class="table-responsive">
+                 <table class="table table-bordered table-condensed table-hover">
 
-                <tbody>
-                    <tr>
+                    <tbody>
+                        <tr>
+                            <th><?php echo Yii::t('t','Rapporteringsdatum');?></th>
+                            <th><?php echo Yii::t('t','Rapporterad av');?></th>
+                            <th><?php echo Yii::t('t','CVId');?></th>
+                            <th><?php echo Yii::t('t','Anledning');?></th>
+                            <th><?php echo Yii::t('t','Radera CV');?></th>
+                            <th><?php echo Yii::t('t','Rapportera CV som OK');?></th>
+                        </tr>
+                        <?php
+                        foreach($allModels as $model){
+                            ?>
+                            <tr id="<?php echo $model->cvId;?>"> 
 
-                        <th><?php echo Yii::t('t','Rapporteringsdatum');?></th>
-                        <th><?php echo Yii::t('t','Rapporterad av');?></th>
-                        <th><?php echo Yii::t('t','CVId');?></th>
-                        <th><?php echo Yii::t('t','Anledning');?></th>
-                        <th><?php echo Yii::t('t','Radera CV');?></th>
-                        <th><?php echo Yii::t('t','Rapportera CV som OK');?></th>
-                    </tr>
-                    <?php
-                    foreach($allModels as $model){
-                        ?>
-                        <tr id="<?php echo $model->cvId;?>"> 
 
-
+                                <td class="onClick"><?php
+                                echo substr($model->date, 0, 10);
+                                ?>
+                            </td>
                             <td class="onClick"><?php
-                            echo substr($model->date, 0, 10);
+                            echo $model->reportedBy;
                             ?>
                         </td>
                         <td class="onClick"><?php
-                        echo $model->reportedBy;
+                        echo $model->cvId;
                         ?>
                     </td>
                     <td class="onClick"><?php
-                    echo $model->cvId;
+                    echo $model->reason;
                     ?>
+                </td> 
+                <td style='text-align:center;'>
+                    <a href =""; return false;> <span id="<?php echo $model->cvId?>" value="<?php echo $model->id?>" class="glyphicon glyphicon-trash deleteReportedCV"></span> </a>
                 </td>
-                <td class="onClick"><?php
-                echo $model->reason;
-                ?>
-            </td> 
-            <td style='text-align:center;'>
-                <a href =""; return false;> <span id="<?php echo $model->cvId?>" value="<?php echo $model->id?>" class="glyphicon glyphicon-trash deleteReportedCV"></span> </a>
-            </td>
-            <td style='text-align:center;'>
-                <a href =""; return false;> <span id="<?php echo $model->id?>" class="glyphicon glyphicon-flag OkReportedCv"></span> </a>
-            </td>
-        </tr>
-        <?php
-    }?>
-</tbody>
+                <td style='text-align:center;'>
+                    <a href =""; return false;> <span id="<?php echo $model->id?>" class="glyphicon glyphicon-flag OkReportedCv"></span> </a>
+                </td>
+            </tr>
+            <?php
+        }?>
+    </tbody>
 </table>
 </div>
 </div>
 </div>
 </div>
 </div>
-
-
-
+</div>
 <script>
 $(".deleteReportedCV").on("click",function(event){
     event.preventDefault();	
